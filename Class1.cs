c4827bd9675e19c8c01f4b0d3eaf723bb541948b
Kaleb Rionda <sergiokaleb.riondamorales@gmail.com>
2023-08-23 12:52:01 -0600

    Add Property to Temperatura

--- Class1.cs
+++ Class1.cs
@@ -9,7 +9,22 @@ namespace ExpendedoraG2_2024_1
         #region Atributos
         private string marca;
         private ushort cantproductos;
+        private byte temperatura;
         private float precio;
+
+        #endregion
+
+        #region Propiedades
+        public byte Temperatura { 
+            get => temperatura; //limites de lectura
+            set
+            {
+                if (0 < value && value < 25)
+                    temperatura = value; //limites de escritura
+                else
+                    temperatura = 20;
+            }
+        }
         #endregion
 
         #region Métodos
@@ -74,8 +89,16 @@ namespace ExpendedoraG2_2024_1
 
         public Expendedora(bool Mantenimiento)
         {
+            Temperatura = 20;
             if ( Mantenimiento == true)
                 Console.WriteLine("Entrando en modo mantenimiento");
+            Console.WriteLine("Cambiando temperatura");
+            LimpiarDisplay();
+            for (int i =0; i < 20;i++)
+            {
+                Temperatura++;
+            }
+            Console.WriteLine("Mostrando Temperatura {0} [°C]",Temperatura);
         }
 
 
